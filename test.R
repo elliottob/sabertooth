2164e2b54442efd098ece892d1f88166cb70d5a2
elliott o'brien <elliottob@gmail.com>
2018-02-11 16:32:46 -0800

    some basic dplyr workflow

--- test.R
+++ test.R
@@ -1,4 +1,28 @@
 # test.R
 
+# tidyverse
+library('tidyverse')
+
 # import data
 load("Baseball.RData")
+
+Batting %>%
+  filter(SB > 1) %>%
+  select(playerID, yearID, SB) %>%
+  group_by(playerID) %>%
+  summarise(
+    SB.mean = mean(SB, na.rm=T),
+    SB.total = sum(SB, na.rm=T)
+  ) %>%
+  arrange(desc(SB.total))
+
+
+Batting %>% filter(SB>1) %>% summarise(mean = mean(SB, na.rm=T))
+
+Batting %>% 
+  filter(SB > 1) %>%
+  group_by(playerID) %>%
+  mutate(
+    SB.deviation = SB - mean(SB, na.rm=T)
+) %>% select(playerID, SB.deviation) 
+
