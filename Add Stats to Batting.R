aa43ce18f73796d9db3ed2ff88015a921e3550b0
ryan pelkey <rpelkey@gmail.com>
2018-02-19 18:11:05 -0800

    removed subsets added plan notes

--- Add Stats to Batting.R
+++ Add Stats to Batting.R
@@ -1,7 +1,11 @@
 ## Add BA, OBP and SLG to Batting data
+library(dplyr)
 
 Batting$BA <- with(Batting, H / AB)
 Batting$OBP <- with(Batting, (H + BB + IBB + HBP) / (AB + BB + IBB + HBP + SF))
 Batting$SLG <- with(Batting, (H - X2B - X3B - HR + 2 * X2B + 3 * X3B + 4 * HR) / AB)
-Batting.forBAProjection <- subset(Batting, yearID >= 2012)
-Batting.forBAProjection
+
+## create unique player vector of who played in 2016
+## add birth year; see marchi p188
+## create lm function for BA, OBP and SLG
+## use sapply with lm function on the player list
